469c5b48f079b2f46ca7d0d45c8108c91c7f34dc
Osama Shahid <osama_o.s@hotmail.com>
2026-03-30 21:10:08 -0400

    implemented import_to_db function in n_port_importer class

--- etf_filing_system/data_imports/n_port_importer.py
+++ etf_filing_system/data_imports/n_port_importer.py
@@ -1,9 +1,8 @@
-try:
-    from .importer import DataImporter
-except ImportError:
-    from importer import DataImporter
+from .importer import DataImporter
 import pandas as pd
 from fastapi import UploadFile
+from sqlmodel import Session, select
+from ..models import get_engine, Holdings, Filings
 
 class NPortImporter(DataImporter):
     COLUMN_MAP = {
@@ -23,7 +22,7 @@ class NPortImporter(DataImporter):
 
     def __init__(self):
         self.df = None
-    
+
     async def parsefile(self, file: UploadFile):
         await file.seek(0)
         df = pd.read_csv(file.file, delimiter="\t")
@@ -53,4 +52,35 @@ class NPortImporter(DataImporter):
         )
         standardized = standardized[self.HOLDINGS_COLUMNS]
         return standardized
-    
+
+    def import_to_db(self):
+        with Session(get_engine()) as session:
+            for record in self.df.to_records('dict'):
+                accession_number = record.get('ACCESSION_NUMBER', None)
+
+                filing = session.exec(
+                    select(Filings).where(Filings.accession_number == accession_number)
+                ).first()
+
+                if filing is None:
+                    continue
+
+                holdings = session.exec(
+                    select(Holdings).where(Holdings.filing_id == filing.id)
+                ).first()
+
+                if holdings is None:
+                    holdings = Holdings(
+                        filing_id=filing.id,
+                        issuer_name=record.get('issuer_name', None),
+                        cusip=record.get('cusip', None),
+                        lei=record.get('lei', None),
+                        market_value=record.get('market_value', 0.0),
+                        weight_pct=record.get('weight_pct', 0.0),
+                        asset_category=record.get('asset_category', None),
+                        issuer_type=record.get('issuer_type', None),
+                        country=record.get('country', None)
+                    )
+                
+                session.add(holdings)
+            session.commit()
