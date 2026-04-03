92572a0bafa12c1985988a414e289f83f1e807ed
Osama Shahid <osama_o.s@hotmail.com>
2026-04-03 19:15:00 -0400

    added seperate route for n-port file to process through celery

--- etf_filing_system/app.py
+++ etf_filing_system/app.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi import UploadFile
 from contextlib import asynccontextmanager
+from sqlmodel import Session
+from .models import ImportJob, save_file_to_store, get_session
 from .schema import ETFFundFormData
 from .data_imports.import_factory import ImportFactory
+from .status import ImportJobStatus
+from .task import process_nport_files
+
 
 @asynccontextmanager
 async def on_startup(app: FastAPI, debug=True):
@@ -23,6 +28,27 @@ async def create_fund_type(fund_data: ETFFundFormData):
     except Exception:
         print("")
     pass
+
+@app.post('/upload/n-port')
+async def create_import(file: UploadFile, session: Session=Depends(get_session)):
+    try:
+        stored_path = save_file_to_store(file)
+        job = ImportJob(
+            source_filename=file.filename,
+            stored_file=stored_path, 
+            status=ImportJobStatus.QUEUED.value
+        )
+        session.add(job)
+        session.commit()
+        session.refresh(job)
+
+        process_nport_files.delay(job.id)
+
+    except Exception as er:
+        return HTTPException(status_code=400, detail="Import Job failed to import N-Port File.")
+
+    return {"job_id": job.id, "status": job.status}
+
 
 @app.post("/upload")
 async def upload_file(file: UploadFile, fund_type:str):
